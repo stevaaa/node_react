d79401dfd18d013b65872fe0441272fcfb1e1673
Stephen Samuel <stephensamuel@Stephens-MacBook-Pro.local>
2018-07-31 12:45:42 +0200

    heroku proxy redirect error fixed (https) by tweaking googleStrategy

--- services/passport.js
+++ services/passport.js
@@ -17,11 +17,12 @@ passport.deserializeUser((id, done) => {
 });
 
 passport.use(
-	new googleStrategy(
+	new GoogleStrategy(
 		{
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback'
+			callbackURL: '/auth/google/callback',
+			proxy: true
 		},
 		(accessToken, refreshToken, profile, done) => {
 			User.findOne({ googleId: profile.id })
